47fca265230fb797ad3b824d27e49bd892ad0578
Juan David Avendaño <150249221+Reants@users.noreply.github.com>
2025-05-26 09:28:00 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+function VentanaFeliz(){
+    window.open("yes.html", "_blank", "width=800,height=600,scrollbars=yes");
+}
+
+function VentanaTriste(){
+    window.open("no.html", "_blank", "width=800,height=600,scrollbars=yes");
+}
